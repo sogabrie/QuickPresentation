52f3060d85d4d24dc2d54728ec75e15be9be62a4
sogabrie <gabrielyan.sos@mail.ru>
2024-05-22 16:30:16 +0400

    update

--- Module_00/ex00/DivideAndRule.cpp
+++ Module_00/ex00/DivideAndRule.cpp
@@ -1,4 +1,64 @@
 #include "DivideAndRule.hpp"
 
+const int Bank::Account::getValue()
+{
+	return 0;
+}
 
+const int Bank::Account::getId()
+{
+	return 0;
+}
 
+const int Bank::Account::getCredit()
+{
+	return 0;
+}
+
+Bank::Bank()
+{
+}
+
+Bank::~Bank()
+{
+}
+
+int &Bank::operator[](int id)
+{
+	// TODO: вставьте здесь оператор return
+}
+
+const int &Bank::getLiquidity()
+{
+	// TODO: вставьте здесь оператор return
+}
+
+const int &Bank::getClientAccounts()
+{
+	// TODO: вставьте здесь оператор return
+}
+
+void Bank::addAccaunt(const int id, int value)
+{
+}
+
+void Bank::addMoney(const int id)
+{
+}
+
+void Bank::getMoney(const int id, const int count)
+{
+}
+
+void Bank::addCredit(const int id, const int count)
+{
+}
+
+void Bank::deleetAccaunt(const int id)
+{
+}
+
+std::ostream &operator<<(std::ostream &p_os, const Bank &p_bank)
+{
+	// TODO: вставьте здесь оператор return
+}
--- Module_00/ex00/DivideAndRule.hpp
+++ Module_00/ex00/DivideAndRule.hpp
@@ -3,46 +3,62 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 
-struct Account
+
+
+class Bank
 {
-	int id;
-	int value;
-
-
-	Account() :
-		id(-1),
-		value(0)
+private:
+	class Account
 	{
-	
-	}
+	public:
+		int	_id;
+		int	_credit;
+		int	_value;
 
-	friend std::ostream& operator << (std::ostream& p_os, const Account& p_account)
-	{
-		p_os << "[" << p_account.id << "] - [" << p_account.value << "]";
-		return (p_os);
-	}
-};
+		Account(const int id, const int value);
 
-struct Bank
-{
-	int liquidity;
-	std::vector<Account *> clientAccounts;
+		const int	getValue();
+		const int	getId();
+		const int	getCredit();
 
-	Bank() :
-		liquidity(0)
-	{
+		friend std::ostream& operator << (std::ostream& p_os, const Account& p_account);
+		// {
+		// 	p_os << "[" << p_account._id << "] - [" << p_account._value << "]";
+		// 	return (p_os);
+		// }
+	};
+private:
+	int _liquidity;
+	// std::vector<Account *> clientAccounts;
+	std::map<int, Account *> _clientAccounts;
+public:
+	Bank();
+	~Bank();
 
-	}
+	int &	operator[](int id);
 
-	friend std::ostream& operator << (std::ostream& p_os, const Bank& p_bank)
-	{
-		p_os << "Bank informations : " << std::endl;
-		p_os << "Liquidity : " << p_bank.liquidity << std::endl;
-		for (auto &clientAccount : p_bank.clientAccounts)
-        p_os << *clientAccount << std::endl;
-		return (p_os);
-	}
+	const int & getLiquidity();
+	const int & getClientAccounts();
+
+	void	addAccaunt(const int id, int value);
+	void	addMoney(const int id);
+	void	getMoney(const int id, const int count);
+
+	void	addCredit(const int id, const int count);
+	void	deleetAccaunt(const int id);
+
+
+
+	friend std::ostream& operator << (std::ostream& p_os, const Bank& p_bank);
+	// {
+	// 	p_os << "Bank informations : " << std::endl;
+	// 	p_os << "Liquidity : " << p_bank.liquidity << std::endl;
+	// 	for (auto &clientAccount : p_bank.clientAccounts)
+    //     p_os << *clientAccount << std::endl;
+	// 	return (p_os);
+	// }
 };
 
 #endif
