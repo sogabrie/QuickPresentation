514f5383914b1e2160b6225d56c665f5f986d100
Sos Gabrielyan <sogabrie@c2r18s4.42yerevan.am>
2025-04-22 22:21:16 +0400

    update

--- Module_03/ex00/car.hpp
+++ Module_03/ex00/car.hpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#define SPEEDMAX 360
 
 class Engine{
 public:
+
+    Engine()
+    {
+        this->_eng = false;
+    }
+
     void    start()
     {
         _eng = true;
@@ -18,6 +25,7 @@ public:
     {
         return _eng;
     }
+
 private:
     bool    _eng;
 };
@@ -27,4 +35,59 @@ class Speed
 {
 public:
 
+    Speed(Engine *engien)
+    {
+        this->_engien = engien; 
+        this->_speed = 0;
+    }
+
+    void accelerate(int speed)
+    {
+
+        if (speed + this->_speed <= SPEEDMAX && speed + this->_speed >= 0) 
+        {
+            this->_speed += speed;
+        }else
+        {
+            if (speed > 0)
+            {
+                this->_speed = SPEEDMAX;
+            }else
+            {
+                this->_speed = 0;
+            }
+        }
+        std::cout << "Spedd = " << this->_speed << std::endl;
+    }
+
+    int getSpeed()
+    {
+        return this->_speed;
+    }
+
+private:
+
+    int _speed;
+    Engine *_engien;
+
 };
+
+class Transmission
+{
+public:
+
+private:
+}
+
+class SteeringWheel
+{
+public:
+
+private:
+}
+
+class Brakes
+{
+public:
+private:
+}
