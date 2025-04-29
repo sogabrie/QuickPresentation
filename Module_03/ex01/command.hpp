9aa67e608ad3c43c359e07f31b84d0fe567c23b2
sogabrie <gabrielyan.sos@mail.ru>
2025-04-29 22:03:14 +0400

    Update

--- Module_03/ex01/command.hpp
+++ Module_03/ex01/command.hpp
@@ -1,15 +1,42 @@
 #ifndef _COMMAND_CPP_
 #define _COMMAND_CPP_
 
-class command
-{
-private:
+#include <string>
 
+class  Articles
+{
 public:
 
-    unsigned int id;
-    
+    Articles(std::string name, double price) : _name(name), _price(price)
+    {
 
+    }
+
+    std::string getName()
+    {
+        return this->_name;
+    }
+
+    double  getPrice()
+    {
+        return this->_price;
+    }
+
+private:
+    std::string _name;
+    double      _price;
+};
+
+class command
+{
+public:
+
+private:
+
+    unsigned int    _id;
+    std::string     _date;
+    std::string     _client;
+    Articles        _articles;
 };
 
 #endif
